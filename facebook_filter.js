8f926075245faab7c744de5b9ece8a34cd05f161
Derek Story <derek.a.story@gmail.com>
2015-08-25 09:45:36 -0400

    now using MutationObserver to detect DOM changes

--- facebook_filter.js
+++ facebook_filter.js
@@ -1,31 +1,26 @@
 $(document).ready(function() {
-    
-    //
-    // INITIALIZE
-    //
-    removeTrump();
 
-    
-    //
-    // Remove any post on the home page
-    // that contain any combination of
-    // "donald" && "trump"
-    //
-    function removeTrump() {
-        $('.userContentWrapper').each(function() {
-            var post = $(this);
-            var postText = post.text();
-            var postMatch = postText.toLowerCase().indexOf("donald") >= 0 && postText.toLowerCase().indexOf("trump") >= 0;
-            if(postMatch) {
-                post.hide();
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            var newNodes = mutation.addedNodes;
+            if (newNodes !== null) { 
+                $('.userContentWrapper').each(function() {
+                    var post = $(this);
+                    var postText = post.text();
+                    var postMatch = postText.toLowerCase().indexOf("donald") >= 0 && postText.toLowerCase().indexOf("trump") >= 0;
+                    if(postMatch) {
+                        post.hide();
+                    }
+                });
             }
         });
-    }
+    });
 
-
-    //
-    // Run function constantly due to infinite scroll
-    //
-    setInterval(removeTrump, 200);
-
+    observer.observe(document, {
+        childList: true,
+        subtree: true,
+        attributes: false,
+        characterData: false,
+    });
+    
 });
