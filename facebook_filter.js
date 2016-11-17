bc52968e9192b33b83ea8a43e438bf858f6c3ca5
Derek Story <derek.a.story@gmail.com>
2016-11-16 20:04:45 -0500

    only search for trump

--- facebook_filter.js
+++ facebook_filter.js
@@ -2,13 +2,13 @@
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             var newNodes = mutation.addedNodes;
-            if (newNodes !== null) {                
-                
+            if (newNodes !== null) {
+
                 var nodes = document.querySelectorAll('.userContentWrapper, ._1bar, ._5my2, ._4qjp, ._2kg4');
                 for (var ii = 0, nn = nodes.length; ii < nn; ii++)
                 {
                     var text = nodes[ii] ? nodes[ii].textContent.toLowerCase() : '';
-                    if (text && text.indexOf('donald') >= 0 && text.indexOf('trump') >= 0 && nodes[ii].style.display != 'none')
+                    if (text && text.indexOf('trump') >= 0 && nodes[ii].style.display != 'none')
                     {
                         nodes[ii].style.display = 'none';
                         chrome.runtime.sendMessage({action: "removeTrump"});
